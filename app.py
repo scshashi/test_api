5471fe44104cfa0a21860add7db50d05dbce3801
scshashi <shashidharascshashi@gmail.com>
2026-04-01 11:23:31 +0530

    Update status message in JSON response

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_posts():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "successful shashi",
+        "status": "successfully auto deployed",
         "status_code": 200
     })
 
